4af6f87c25be6c5ebff4cba1500025a951e8c65d
AndrezNinno <ing.andres.david23@gmail.com>
2021-02-19 22:13:16 -0500

    Se agregaron las clases:
    SecurityConfiguration: Encargada de ser el adaptador en la capa de los filtros como activador de la seguridad web
    JwtRequestFilter: Filtro empleado por la configuración de la seguridad, encargado directamente de identificar el usuario por medio del desencriptamiento del jwt
    
    Se agregó la funcionalidad de la seguridad web en el proyecto junto con la implementación de JWT.

--- src/main/java/com/andrezninno/practice/security/filters/JwtRequestFilter.java
+++ src/main/java/com/andrezninno/practice/security/filters/JwtRequestFilter.java
@@ -8,9 +8,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.andrezninno.practice.domain.BibliotecarioDetails;
+import com.andrezninno.practice.services.BibliotecarioService;
 import com.andrezninno.practice.util.JwtUtil;
 
 @Component
@@ -18,11 +24,32 @@ public class JwtRequestFilter extends OncePerRequestFilter{
 	
 	@Autowired
 	private JwtUtil jwtUtil;
+	
+	@Autowired
+	private BibliotecarioService bibliotecarioService;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		final String authorizationHeader = request.getHeader("Authorization");
+		
+		String username = null;
+		String jwt = null;
+		
+		if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+			jwt = authorizationHeader.substring(7);
+			username = jwtUtil.extractUsername(jwt);
+		}
+		
+		if(username != null && SecurityContextHolder.getContext().getAuthentication() == null ) {
+			UserDetails bibliotecario = this.bibliotecarioService.loadUserByUsername(username);
+			if(jwtUtil.validateToken(jwt, bibliotecario)) {
+				UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(bibliotecario, null, bibliotecario.getAuthorities());
+				token.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+				SecurityContextHolder.getContext().setAuthentication(token);
+			}
+		}
 		
 	}
 	
